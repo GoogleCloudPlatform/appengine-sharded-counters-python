9c44ecf24577e54981f8f7b2a76f8467b074bb66
Daniel Hermes <dhermes@google.com>
2013-01-13 22:29:58 -0800

    Indent only (PEP8) update of general counter.

--- general_counter.py
+++ general_counter.py
@@ -23,62 +23,62 @@ from google.appengine.ext import ndb
 
 
 class GeneralCounterShardConfig(ndb.Model):
-  """Tracks the number of shards for each named counter."""
-  num_shards = ndb.IntegerProperty(required=True, default=20)
+    """Tracks the number of shards for each named counter."""
+    num_shards = ndb.IntegerProperty(required=True, default=20)
 
 
 class GeneralCounterShard(ndb.Model):
-  """Shards for each named counter"""
-  count = ndb.IntegerProperty(required=True, default=0)
+    """Shards for each named counter"""
+    count = ndb.IntegerProperty(required=True, default=0)
 
 
 def get_count(name):
-  """Retrieve the value for a given sharded counter.
+    """Retrieve the value for a given sharded counter.
 
-  Args:
-    name: The name of the counter.
-  """
-  total = memcache.get(name)
-  if total is None:
-    total = 0
-    ancestor_key = ndb.Key(GeneralCounterShard, name)
-    for counter in GeneralCounterShard.query(ancestor=ancestor_key):
-      total += counter.count
-    memcache.add(name, total, 60)
-  return total
+    Args:
+        name: The name of the counter.
+    """
+    total = memcache.get(name)
+    if total is None:
+        total = 0
+        ancestor_key = ndb.Key(GeneralCounterShard, name)
+        for counter in GeneralCounterShard.query(ancestor=ancestor_key):
+            total += counter.count
+        memcache.add(name, total, 60)
+    return total
 
 
 @ndb.transactional
 def increment(name):
-  """Increment the value for a given sharded counter.
+    """Increment the value for a given sharded counter.
 
-  Args:
-    name: The name of the counter.
-  """
-  config = GeneralCounterShardConfig.get_or_insert(name)
+    Args:
+        name: The name of the counter.
+    """
+    config = GeneralCounterShardConfig.get_or_insert(name)
 
-  index = random.randint(0, config.num_shards - 1)
-  shard_key = ndb.Key(GeneralCounterShard, name, GeneralCounterShard, index)
-  counter = shard_key.get()
-  if counter is None:
-    counter = GeneralCounterShard(key=shard_key)
-  counter.count += 1
-  counter.put()
-  # does nothing if the key does not exist
-  memcache.incr(name)
+    index = random.randint(0, config.num_shards - 1)
+    shard_key = ndb.Key(GeneralCounterShard, name, GeneralCounterShard, index)
+    counter = shard_key.get()
+    if counter is None:
+        counter = GeneralCounterShard(key=shard_key)
+    counter.count += 1
+    counter.put()
+    # does nothing if the key does not exist
+    memcache.incr(name)
 
 
 @ndb.transactional
 def increase_shards(name, num_shards):
-  """Increase the number of shards for a given sharded counter.
+    """Increase the number of shards for a given sharded counter.
 
-  Will never decrease the number of shards.
+    Will never decrease the number of shards.
 
-  Args:
-    name: The name of the counter
-    num_shards: How many shards to use
-  """
-  config = GeneralCounterShardConfig.get_or_insert(name)
-  if config.num_shards < num_shards:
-    config.num_shards = num_shards
-    config.put()
+    Args:
+        name: The name of the counter
+        num_shards: How many shards to use
+    """
+    config = GeneralCounterShardConfig.get_or_insert(name)
+    if config.num_shards < num_shards:
+        config.num_shards = num_shards
+        config.put()
